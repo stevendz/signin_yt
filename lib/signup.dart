76a550c10f2d5a9b2d57b16448433f7d97317f16
stevendz <Steven.Dzionara@gmail.com>
2019-10-25 13:32:20 +0200

    yt

---
+++ lib/signup.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomPaint(
+        painter: BackgroundSignUp(),
+        child: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35),
+              child: Column(
+                children: <Widget>[
+                  _getHeader(),
+                  _getTextFields(),
+                  _getSignIn(),
+                  _getBottomRow(context),
+                ],
+              ),
+            ),
+            _getBackBtn()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+_getBackBtn() {
+  return Positioned(
+    top: 35,
+    left: 25,
+    child: Icon(
+      Icons.arrow_back_ios,
+      color: Colors.white,
+    ),
+  );
+}
+
+_getBottomRow(context) {
+  return Expanded(
+    flex: 1,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        GestureDetector(
+          onTap: (){
+            Navigator.pop(context);
+          },
+                  child: Text(
+            'Sign up',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ),
+        Text(
+          '',
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+            decoration: TextDecoration.underline,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+_getSignIn() {
+  return Expanded(
+    flex: 1,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          'Sign up',
+          style: TextStyle(color: Colors.white,fontSize: 25, fontWeight: FontWeight.w500),
+        ),
+        CircleAvatar(
+          backgroundColor: Colors.grey.shade800,
+          radius: 40,
+          child: Icon(
+            Icons.arrow_forward,
+            color: Colors.white,
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+_getTextFields() {
+  return Expanded(
+    flex: 4,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(
+          height: 15,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            labelText: 'Name', labelStyle: TextStyle(color: Colors.white)),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            labelText: 'Email', labelStyle: TextStyle(color: Colors.white)),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            labelText: 'Password', labelStyle: TextStyle(color: Colors.white)),
+        ),
+        SizedBox(
+          height: 25,
+        ),
+      ],
+    ),
+  );
+}
+
+_getHeader() {
+  return Expanded(
+    flex: 3,
+    child: Container(
+      alignment: Alignment.bottomLeft,
+      child: Text(
+        'Create\nAccount',
+        style: TextStyle(color: Colors.white, fontSize: 40),
+      ),
+    ),
+  );
+}
+
+class BackgroundSignIn extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var sw = size.width;
+    var sh = size.height;
+    var paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, sw, sh));
+    paint.color = Colors.grey.shade100;
+    canvas.drawPath(mainBackground, paint);
+
+    Path blueWave = Path();
+    blueWave.lineTo(sw, 0);
+    blueWave.lineTo(sw, sh * 0.5);
+    blueWave.quadraticBezierTo(sw * 0.5, sh * 0.45, sw * 0.2, 0);
+    blueWave.close();
+    paint.color = Colors.lightBlue.shade300;
+    canvas.drawPath(blueWave, paint);
+
+    Path greyWave = Path();
+    greyWave.lineTo(sw, 0);
+    greyWave.lineTo(sw, sh * 0.1);
+    greyWave.cubicTo(
+        sw * 0.95, sh * 0.15, sw * 0.65, sh * 0.15, sw * 0.6, sh * 0.38);
+    greyWave.cubicTo(sw * 0.52, sh * 0.52, sw * 0.05, sh * 0.45, 0, sh * 0.4);
+    greyWave.close();
+    paint.color = Colors.grey.shade800;
+    canvas.drawPath(greyWave, paint);
+
+    Path yellowWave = Path();
+    yellowWave.lineTo(sw * 0.7, 0);
+    yellowWave.cubicTo(
+        sw * 0.6, sh * 0.05, sw * 0.27, sh * 0.01, sw * 0.18, sh * 0.12);
+    yellowWave.quadraticBezierTo(sw * 0.12, sh * 0.2, 0, sh * 0.2);
+    yellowWave.close();
+    paint.color = Colors.orange.shade300;
+    canvas.drawPath(yellowWave, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
+
+class BackgroundSignUp extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var sw = size.width;
+    var sh = size.height;
+    var paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, sw, sh));
+    paint.color = Colors.grey.shade100;
+    canvas.drawPath(mainBackground, paint);
+
+    Path blueWave = Path();
+    blueWave.lineTo(sw, 0);
+    blueWave.lineTo(sw, sh * 0.65);
+    blueWave.cubicTo(sw * 0.8, sh * 0.8, sw * 0.55, sh * 0.8, sw * 0.45, sh);
+    blueWave.lineTo(0, sh);
+    blueWave.close();
+    paint.color = Colors.lightBlue.shade300;
+    canvas.drawPath(blueWave, paint);
+
+    Path greyWave = Path();
+    greyWave.lineTo(sw, 0);
+    greyWave.lineTo(sw, sh * 0.3);
+    greyWave.cubicTo(sw * 0.65, sh * 0.45, sw * 0.25, sh * 0.35, 0, sh * 0.5);
+    greyWave.close();
+    paint.color = Colors.grey.shade800;
+    canvas.drawPath(greyWave, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
